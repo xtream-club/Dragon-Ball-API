7842f09c8f075a2f4d5460df3dcf32220a116c25
xtream_club <107730987+xtream-club@users.noreply.github.com>
2023-05-10 12:10:15 -0600

    Update run.py

--- run.py
+++ run.py
@@ -1,19 +1,8 @@
 from flask import jsonify
-
 from app import create_app
-
-data_info = {"character": "/character/information/name", "planet": "/planet/information/name",
-             "saga": "/saga/information/name", "fusion": "/fusion/information/name",
-             "game": "/videogame/information/name"}
 
 app = create_app()
 app.config['JSON_AS_ASCII'] = False
-
-
-@app.route("/")
-def home():
-    return jsonify(data_info)
-
 
 if __name__ == "__main__":
     from waitress import serve
